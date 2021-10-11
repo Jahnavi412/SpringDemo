5129b11dfe1322da54d68dff1a2dbd027e244fb7
jahnavi <jahnavi.ganjikunta@gmail.com>
2021-10-11 12:10:29 +0530

    constructor for dependency injection

--- src/com/spring/app/BaseballCoach.java
+++ src/com/spring/app/BaseballCoach.java
@@ -1,12 +1,31 @@
 package com.spring.app;
 
 public class BaseballCoach implements Coach {
+    // define private field for the dependency
+    private FortuneService fortuneService;
+
+    // define a constructor for a dependency injection
+    public BaseballCoach(FortuneService fortuneService){
+        fortuneService = fortuneService;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
     @Override
     public String getDailyWorkout() {
         return "spend 30 mintues on walking";
 
     }
-
     @Override
     public String getDailyFortune() {
         return "lucky day";
